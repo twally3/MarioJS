df95a478e4b4abfbf84b6e26bf0623be0161ec19
Max Taylor <max@cakerstream.com>
2017-09-24 21:57:55 +0100

    Added loaders for sprites and refactored. Changes to define in SpriteSheet.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from './SpriteSheet.js'
-import { loadImage, loadLevel } from './loaders.js'
+import { loadLevel } from './loaders.js'
+import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
 
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -11,24 +11,17 @@ function drawBackground(background, context, sprites) {
     })
 }
 
-function loadBackgroundSprites() {
-    return loadImage('/img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image, 16, 16)
-        sprites.define('ground', 0, 0)
-        sprites.define('sky', 3, 23)
-        return sprites
-    })
-}
-
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
 Promise.all([
+    loadMarioSprite(),
     loadBackgroundSprites(),
     loadLevel('1-1')
-]).then(([ sprites, level ]) => {
+]).then(([ marioSprite, sprites, level ]) => {
     level.backgrounds.forEach(background => {
         drawBackground(background, context, sprites)
     })
+
+    marioSprite.draw('idle', context, 64, 64)
 })

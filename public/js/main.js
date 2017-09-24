0140a61464bee04987dc32ace6093e3cf2109794
Max Taylor <max@cakerstream.com>
2017-09-24 22:15:19 +0100

    Refactored and added Compositor to handle image layers. Added Mario Army!

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,43 @@
+import Compositor from './Compositor.js'
 import { loadLevel } from './loaders.js'
 import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
-
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y)
-            }
-        }
-    })
-}
+import { createBackgroundLayer } from './layers.js'
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        for (let i = 0; i < 20; ++i) {
+            sprite.draw('idle', context, pos.x + i * 16, pos.y)
+        }
+    }
+}
 
 Promise.all([
     loadMarioSprite(),
     loadBackgroundSprites(),
     loadLevel('1-1')
-]).then(([ marioSprite, sprites, level ]) => {
-    level.backgrounds.forEach(background => {
-        drawBackground(background, context, sprites)
-    })
+]).then(([ marioSprite, backgroundSprites, level ]) => {
+    const comp = new Compositor()
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites)
+    comp.layers.push(backgroundLayer)
 
-    marioSprite.draw('idle', context, 64, 64)
+    const pos = {
+        x: 0,
+        y: 0
+    }
+
+    const spriteLayer = createSpriteLayer(marioSprite, pos)
+    comp.layers.push(spriteLayer)
+
+    function update() {
+        comp.draw(context)
+        pos.x += 2
+        pos.y += 2
+        requestAnimationFrame(update)
+    }
+
+    update()
+
 })

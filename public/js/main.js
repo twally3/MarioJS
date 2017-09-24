872d844baef3e3ddd220eabd04236269f2d5bdbf
Max Taylor <max@cakerstream.com>
2017-09-24 21:47:56 +0100

    Loaded image and level in parallel (main.js)

--- public/js/main.js
+++ public/js/main.js
@@ -11,19 +11,24 @@ function drawBackground(background, context, sprites) {
     })
 }
 
-const canvas = document.getElementById('screen')
-const context = canvas.getContext('2d')
-
-loadImage('/img/tiles.png')
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
     .then(image => {
         const sprites = new SpriteSheet(image, 16, 16)
         sprites.define('ground', 0, 0)
         sprites.define('sky', 3, 23)
-
-        loadLevel('1-1')
-            .then(level => {
-                level.backgrounds.forEach(background => {
-                    drawBackground(background, context, sprites)
-                })
-            })
+        return sprites
     })
+}
+
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+
+Promise.all([
+    loadBackgroundSprites(),
+    loadLevel('1-1')
+]).then(([ sprites, level ]) => {
+    level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites)
+    })
+})

abdf475e83c2fa9c58416a12f989ee8aef25ea61
Max Taylor <max@cakerstream.com>
2017-10-29 21:02:54 +0000

    Added pendulum walk trait for NPC's. Moved animation logic into mario.json. Added Goomba and Koopa. Cleaned up entity loading in main and made it sync

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,46 +1,18 @@
-import Entity, { Trait } from './Entity.js'
-import Jump from "./traits/Jump.js"
-import Go from "./traits/Go.js"
-import { loadSpriteSheet } from './loaders.js'
-import { createAnim } from './anim.js'
+import { loadMario } from './entities/Mario.js'
+import { loadGoomba } from './entities/Goomba.js'
+import { loadKoopa } from './entities/Koopa.js'
 
-const SLOW_DRAG = 1/1000
-const FAST_DRAG = 1/5000
+export function loadEntities() {
+    const entityFactories = {}
 
-export function createMario() {
-    return loadSpriteSheet('mario')
-        .then(sprite => {
-            const mario = new Entity()
-            mario.size.set(14, 16)
+    function addAs(name) {
+        return factory => entityFactories[name] = factory
+    }
 
-            mario.addTrait(new Go())
-            mario.go.dragFactor = SLOW_DRAG
-
-            mario.addTrait(new Jump())
-            
-            mario.turbo = function setTurboState(turboOn) {
-                this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG
-            }
-
-            const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6)
-            function routeFrame(mario) {
-                if (mario.jump.falling) {
-                    return 'jump'
-                }
-                if (mario.go.distance > 0) {
-                    if ((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
-                        return 'brake'
-                    }
-                    return runAnim(mario.go.distance)
-                }
-
-                return 'idle'
-            }
-        
-            mario.draw = function drawMario(context) {
-                sprite.draw(routeFrame(this), context, 0, 0, mario.go.heading < 0)
-            }
-        
-            return mario
-        })
+    return Promise.all([
+        loadMario().then(addAs('mario')),
+        loadGoomba().then(addAs('goomba')),
+        loadKoopa().then(addAs('koopa'))
+    ])
+    .then(_ => entityFactories)
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,34 @@
 import Camera from './Camera.js'
 import Timer from './Timer.js'
 import { loadLevel } from './loaders/level.js'
-import { createMario } from './entities.js'
+import { loadEntities } from './entities.js'
 import { setupKeyboard } from './input.js'
+// import { createCollisionLayer } from './layers.js'
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
 Promise.all([
-    createMario(),
+    loadEntities(),
     loadLevel('1-1')
-]).then(([ mario, level ]) => {
+]).then(([ entity, level ]) => {
     const camera = new Camera()
     window.camera = camera
 
+    const mario = entity.mario()
     mario.pos.set(64, 64)
+
+    const goomba = entity.goomba()
+    goomba.pos.x = 220
+    level.entities.add(goomba)
+
+    const koopa = entity.koopa()
+    koopa.pos.x = 260
+    level.entities.add(koopa)
     
     level.entities.add(mario)
+
+    // level.comp.layers.push(createCollisionLayer(level))
 
     const input = setupKeyboard(mario)
     input.listenTo(window)

0cb11ab78454ffdaa93c3e4114c805ffb427858f
Max Taylor <max@cakerstream.com>
2017-11-05 20:24:11 +0000

    Used yield* for recursive generators! Refactored and started loading and positioning entities from the levels json

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,26 @@
 import Camera from './Camera.js'
 import Timer from './Timer.js'
-import { loadLevel } from './loaders/level.js'
+import { createLevelLoader } from './loaders/level.js'
 import { loadEntities } from './entities.js'
 import { setupKeyboard } from './input.js'
-// import { createCollisionLayer } from './layers.js'
+import { createCollisionLayer } from './layers.js'
 
-const canvas = document.getElementById('screen')
-const context = canvas.getContext('2d')
+;(async function main (canvas) {
+    const context = canvas.getContext('2d')
+    
+    const entityFactory = await loadEntities()
+    const loadLevel = await createLevelLoader(entityFactory)
+    const level = await loadLevel('1-1')
 
-Promise.all([
-    loadEntities(),
-    loadLevel('1-1')
-]).then(([ entity, level ]) => {
     const camera = new Camera()
     window.camera = camera
 
-    const mario = entity.mario()
+    const mario = entityFactory.mario()
     mario.pos.set(64, 64)
-
-    const goomba = entity.goomba()
-    goomba.pos.x = 220
-    level.entities.add(goomba)
-
-    const koopa = entity.koopa()
-    koopa.pos.x = 260
-    level.entities.add(koopa)
     
     level.entities.add(mario)
 
-    // level.comp.layers.push(createCollisionLayer(level))
+    level.comp.layers.push(createCollisionLayer(level))
 
     const input = setupKeyboard(mario)
     input.listenTo(window)
@@ -44,4 +36,4 @@ Promise.all([
         level.comp.draw(context, camera)
     }
     timer.start()
-})
+})(document.getElementById('screen'))
